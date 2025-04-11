71b4284eca259e0d2d4c2346d3eceb9e9164f90a
Nramirez <nramirez@uma.es>
2025-04-11 18:40:34 +0200

    refactor(app):change structure of the package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="NcViewer",
+    name="ncviewer",
     version="0.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
